7caf525b3419c4ccc5dac7618b9d8643118a9bfe
fabiom93 <63911405+fabiom93@users.noreply.github.com>
2021-10-15 13:06:00 +0200

    Update functions

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,12 +1,15 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
 import './App.scss';
+import CustomButton from '../components/custom-button/CustomButton';
 
 const App = () => {
   const message = 'React wpp';
   return (
     <div className="app">
-      <h1>{message}</h1>
+      <CustomButton onClick={() => 2 * 2} variant="primary" size="medium">
+        {message}
+      </CustomButton>
     </div>
   );
 };
