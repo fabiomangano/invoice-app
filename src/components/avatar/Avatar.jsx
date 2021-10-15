3bb47fe72589d8fe56fdfe1f7839a80f2bfce950
fabiom93 <63911405+fabiom93@users.noreply.github.com>
2021-10-15 15:22:15 +0200

    Create Avatar Component

---
+++ src/components/avatar/Avatar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import placeHolderAvatar from '../../assets/image-avatar.jpg';
+import './Avatar.style.scss';
+
+const Avatar = ({ src, size, alt }) => (
+  <img className="avatar" src={src} alt={alt} size={size} />
+);
+
+Avatar.defaultProps = {
+  size: 'medium',
+  src: placeHolderAvatar,
+};
+
+Avatar.propTypes = {
+  src: PropTypes.string,
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  alt: PropTypes.string.isRequired,
+};
+
+export default Avatar;
