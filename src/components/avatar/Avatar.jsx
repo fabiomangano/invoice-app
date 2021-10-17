ef0bd43555a0cf932fb2590e6d594ab1d5fa03e0
fabiom93 <63911405+fabiom93@users.noreply.github.com>
2021-10-17 11:49:26 +0200

    Remove useless size props

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -3,18 +3,14 @@ import PropTypes from 'prop-types';
 import placeholderAvatar from '../../assets/image-avatar.jpg';
 import './Avatar.style.scss';
 
-const Avatar = ({ src, size, alt }) => (
-  <img className="avatar" src={src} alt={alt} size={size} />
-);
+const Avatar = ({ src, alt }) => <img className="avatar" src={src} alt={alt} />;
 
 Avatar.defaultProps = {
-  size: 'medium',
   src: placeholderAvatar,
 };
 
 Avatar.propTypes = {
   src: PropTypes.string,
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
   alt: PropTypes.string.isRequired,
 };
 
