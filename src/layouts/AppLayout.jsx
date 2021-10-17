1a57fa9567ec9d3f056ad9d1fb5a03d5ca55ced9
fabiom93 <63911405+fabiom93@users.noreply.github.com>
2021-10-17 23:28:02 +0200

    Move responsive logic out from component

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,40 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cn from 'classnames';
-import { useMediaQuery } from 'react-responsive';
-import { mobile } from '../mediaQueries';
 import Appbar from '../components/appbar/Appbar';
 import ToggleTheme from '../components/toggle-theme/ToggleTheme';
 import Avatar from '../components/avatar/Avatar';
 import Logo from '../components/logo/Logo';
 import './AppLayout.style.scss';
 
-const AppLayout = ({ children }) => {
-  const isMobile = useMediaQuery(mobile);
-  const orientation = isMobile
-    ? 'applayout--vertical'
-    : 'applayout--horizontal';
-  return (
-    <div className={cn('applayout', orientation)}>
-      <Appbar variant={isMobile ? 'horizontal' : 'vertical'}>
-        <Logo />
-        <Appbar.Menu>
-          <Appbar.Option>
-            <ToggleTheme />
-          </Appbar.Option>
-          <Appbar.Divider />
-          <Appbar.Option>
-            <Avatar />
-          </Appbar.Option>
-        </Appbar.Menu>
-      </Appbar>
-      {children}
-    </div>
-  );
-};
+const AppLayout = ({ children, isMobile }) => (
+  <div
+    className={cn(
+      'applayout',
+      isMobile ? 'applayout--vertical' : 'applayout--horizontal',
+    )}
+  >
+    <Appbar variant={isMobile ? 'horizontal' : 'vertical'}>
+      <Logo />
+      <Appbar.Menu>
+        <Appbar.Option>
+          <ToggleTheme />
+        </Appbar.Option>
+        <Appbar.Divider />
+        <Appbar.Option>
+          <Avatar />
+        </Appbar.Option>
+      </Appbar.Menu>
+    </Appbar>
+    {children}
+  </div>
+);
 
 AppLayout.propTypes = {
   children: PropTypes.node.isRequired,
+  isMobile: PropTypes.bool.isRequired,
 };
 
 export default AppLayout;
