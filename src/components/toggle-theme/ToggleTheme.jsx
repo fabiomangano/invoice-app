d627b94ef0d258e296f3d23bb1fbfc479f1c4699
fabiom93 <63911405+fabiom93@users.noreply.github.com>
2021-10-17 22:48:32 +0200

    Create ToggleTheme component

--- src/components/toggle-theme/ToggleTheme.jsx
+++ src/components/toggle-theme/ToggleTheme.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ReactComponent as LightModeIcon } from '../../assets/logo.svg';
+import { ReactComponent as LightModeIcon } from '../../assets/icon-moon.svg';
 import { ReactComponent as DarkModeIcon } from '../../assets/icon-sun.svg';
+import './ToggleTheme.style.scss';
 
 const ToggleTheme = ({ theme, setTheme }) => (
-  <button type="button" setTheme={setTheme}>
+  <button className="toggle-theme" type="button" onClick={setTheme}>
     {theme === 'light' ? <DarkModeIcon /> : <LightModeIcon />}
   </button>
 );
 
 ToggleTheme.propTypes = {
-  theme: PropTypes.oneOf('light', 'dark').isRequired,
+  theme: PropTypes.oneOf(['light', 'dark']).isRequired,
   setTheme: PropTypes.func.isRequired,
 };
 
