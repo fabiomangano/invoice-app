4f60bfb418e1d9e11a895768f75784da4eb7d7e9
fabiom93 <63911405+fabiom93@users.noreply.github.com>
2021-10-18 20:01:30 +0200

    Update ToggleTheme component

--- src/components/toggle-theme/ToggleTheme.jsx
+++ src/components/toggle-theme/ToggleTheme.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ReactComponent as LightModeIcon } from '../../assets/icon-moon.svg';
-import { ReactComponent as DarkModeIcon } from '../../assets/icon-sun.svg';
+import { ReactComponent as DarkModeIcon } from '../../assets/icon-moon.svg';
+import { ReactComponent as LightModeIcon } from '../../assets/icon-sun.svg';
 import './ToggleTheme.style.scss';
 
-const ToggleTheme = ({ theme, setTheme }) => (
-  <button className="toggle-theme" type="button" onClick={setTheme}>
+const ToggleTheme = ({ theme, toggleTheme }) => (
+  <button className="toggle-theme" type="button" onClick={toggleTheme}>
     {theme === 'light' ? <DarkModeIcon /> : <LightModeIcon />}
   </button>
 );
 
 ToggleTheme.propTypes = {
   theme: PropTypes.oneOf(['light', 'dark']).isRequired,
-  setTheme: PropTypes.func.isRequired,
+  toggleTheme: PropTypes.func.isRequired,
 };
 
 export default ToggleTheme;
--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,34 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cn from 'classnames';
+import { ThemeContext } from '../components/theme-provider/ThemeProvider';
 import Appbar from '../components/appbar/Appbar';
 import ToggleTheme from '../components/toggle-theme/ToggleTheme';
 import Avatar from '../components/avatar/Avatar';
 import Logo from '../components/logo/Logo';
 import './AppLayout.style.scss';
 
-const AppLayout = ({ children, isMobile }) => (
-  <div
-    className={cn(
-      'applayout',
-      isMobile ? 'applayout--vertical' : 'applayout--horizontal',
-    )}
-  >
-    <Appbar variant={isMobile ? 'horizontal' : 'vertical'}>
-      <Logo />
-      <Appbar.Menu>
-        <Appbar.Option>
-          <ToggleTheme />
-        </Appbar.Option>
-        <Appbar.Divider />
-        <Appbar.Option>
-          <Avatar />
-        </Appbar.Option>
-      </Appbar.Menu>
-    </Appbar>
-    {children}
-  </div>
-);
+const AppLayout = ({ children, isMobile }) => {
+  const { theme, toggleTheme } = React.useContext(ThemeContext);
+  return (
+    <div
+      className={cn(
+        'applayout',
+        isMobile ? 'applayout--vertical' : 'applayout--horizontal',
+      )}
+    >
+      <Appbar variant={isMobile ? 'horizontal' : 'vertical'}>
+        <Logo />
+        <Appbar.Menu>
+          <Appbar.Option>
+            <ToggleTheme theme={theme} toggleTheme={toggleTheme} />
+          </Appbar.Option>
+          <Appbar.Divider />
+          <Appbar.Option>
+            <Avatar alt="Profile Image" />
+          </Appbar.Option>
+        </Appbar.Menu>
+      </Appbar>
+      <div className={cn('applayout', 'applayout--vertical')}>{children}</div>
+    </div>
+  );
+};
 
 AppLayout.propTypes = {
   children: PropTypes.node.isRequired,
