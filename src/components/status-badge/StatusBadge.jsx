5fad66d0ae9eea60941e349193323d9ac0fa5d97
fabiom93 <63911405+fabiom93@users.noreply.github.com>
2021-10-19 17:58:02 +0200

    Create StatusBadge component

---
+++ src/components/status-badge/StatusBadge.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cn from 'classnames';
+import './StatusBadge.style.scss';
+
+const StatusBadge = ({ children, variant }) => (
+  <div className={cn('status-badge', `status-badge__background--${variant}`)}>
+    <div
+      className={cn(`status-badge__circle`, `status-badge__circle--${variant}`)}
+    />
+    <span
+      className={(`status-badge__label`, `status-badge__label--${variant}`)}
+    >
+      {children}
+    </span>
+  </div>
+);
+
+StatusBadge.propTypes = {
+  children: PropTypes.string.isRequired,
+  variant: PropTypes.oneOf(['paid', 'pending', 'draft']).isRequired,
+};
+
+export default StatusBadge;
