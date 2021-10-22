ddb9e2a404e50b1906012bd702193117422ea4d6
fabiom93 <63911405+fabiom93@users.noreply.github.com>
2021-10-22 23:47:31 +0200

    Create test for logo component

---
+++ src/components/logo/Logo.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Renderer from 'react-test-renderer';
+import Logo from './Logo';
+
+describe('Logo', () => {
+  it.skip('Should render correctly the main app logo', () => {
+    let tree;
+    Renderer.act(() => {
+      tree = Renderer.create(<Logo />);
+    });
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
