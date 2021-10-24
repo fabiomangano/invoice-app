889e57fe85f4e50d8207a160647a8b72b6d8dd78
fabiom93 <63911405+fabiom93@users.noreply.github.com>
2021-10-24 22:03:03 +0200

    Update App Component

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,22 +1,18 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
-import { useMediaQuery } from 'react-responsive';
-import { mobile } from '../mediaQueries';
 import ThemeProvider from '../components/theme-provider/ThemeProvider';
 import AppLayout from '../layouts/AppLayout';
 import Home from '../pages/Home';
 import './App.scss';
 
-const App = () => {
-  const isMobile = useMediaQuery(mobile);
-  return (
-    <ThemeProvider>
-      <div className="app">
-        <AppLayout isMobile={isMobile}>
-          <Home />
-        </AppLayout>
-      </div>
-    </ThemeProvider>
-  );
-};
+const App = () => (
+  <ThemeProvider>
+    <div className="app">
+      <AppLayout>
+        <Home />
+      </AppLayout>
+    </div>
+  </ThemeProvider>
+);
+
 export default hot(App);
