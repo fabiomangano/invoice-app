7ef2202ce57988173e13d1610e18aceb9e43ac9b
fabiom93 <63911405+fabiom93@users.noreply.github.com>
2021-10-24 22:05:22 +0200

    Update Home Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import StatusBadge from '../components/status-badge/StatusBadge';
+import FilterableList from '../components/invoice-filterable-list/FilterableList';
+import Button from '../components/button/Button';
+import { ReactComponent as IconPlus } from '../assets/icon-plus.svg';
+import invoices from '../../db';
 import './Home.style.scss';
 
 const Home = () => (
   <div className="home">
-    <StatusBadge variant="paid">pending</StatusBadge>
+    <FilterableList data={invoices}>
+      <FilterableList.Header>
+        <FilterableList.Counter />
+        <FilterableList.ActionGroup>
+          <FilterableList.Filter />
+          <Button variant="primary">
+            <IconPlus />
+            Add
+          </Button>
+        </FilterableList.ActionGroup>
+      </FilterableList.Header>
+      <FilterableList.List />
+    </FilterableList>
   </div>
 );
 
